60a68842685c243424228b150d2ad4feaac36374
Rayhan AL <78718540+Ryhan444@users.noreply.github.com>
2026-03-23 21:23:28 +0700

    [upd] revision

--- ara_face_attendance/__manifest__.py
+++ ara_face_attendance/__manifest__.py
@@ -4,7 +4,7 @@
     "category": "Tools",
     "summary": """Face Identification login Attendance""",
     "author": "ARA SOFT",
-    "depends": ["ara_base_identify_face", "hr_attendance"],
+    "depends": [ "hr_attendance", "ara_base_identify_face"],
     "assets": {
         "web.assets_backend": [
             "ara_base_identify_face/static/face-api/face-api.js",
@@ -13,13 +13,16 @@
             "ara_base_identify_face/static/face-api/face-api.js",
             "ara_face_attendance/static/src/xml/face_identification.xml",
             "ara_face_attendance/static/src/js/face_identification.js",
+            "ara_face_attendance/static/src/xml/manual_selection.xml",
+            "ara_face_attendance/static/src/js/manual_selection.js",
         ],
     },
     "license": "OPL-1",
     "installable": True,
     "auto_install": False,
     "application": False,
-    "price": 58.22,
+    "price": 48.22,
     "currency": "USD",
     "images": ['static/description/banner.gif'],
+
 }
